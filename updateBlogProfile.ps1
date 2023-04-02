8abb8876d2240ff92ee875be27e2f1ada3f7d7d5
Jeevith Hegde <jeevith20@gmail.com>
2023-04-02 17:55:39 +0200

    Updated profile

--- updateBlogProfile.ps1
+++ updateBlogProfile.ps1
@@ -1,3 +1,6 @@
 $UserName="jeev20"
-gitfolio build $UserName --theme dark
+gitfolio build $UserName --theme dark  --sort star --order "des"
 gitfolio update
+
+Get-ChildItem -Path "./dist" | Move-Item -Destination "./jeev20.github.io" -Force
+Remove-Item "./dist" -Confirm:$false -Force
